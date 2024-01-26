827dec3d9932545f251a3b1b4bf55c11aac59323
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2024-01-26 22:22:53 +0000

    tr-script.js-updated code adding function to compile the message using the object array of messages and the random number generated

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 console.log('Working')
 
 const generateRandomNumber = () => {
-    const randomNumber = Math.floor(Math.random() * 5);
+    const randomNumber = Math.floor(Math.random() * 3);
     return randomNumber;
+};
+
+const messages = {
+    quotes: ["Believe in yourself.", "The only limit is your mind.", "Success is a journey, not a destination."],
+    advice: ["Take one step at a time.", "Embrace challenges as opportunities.", "Learn from your mistakes."],
+    affirmations: ["I am capable of achieving my goals.", "I attract positivity into my life.", "I am resilient and strong."],
+};
+
+const messageSelector = (idx, objArrays) => {
+    return `Your quote is: ${objArrays.quotes[idx]}. The advice for you today is ${objArrays.advice[idx]}. Your affirmation is ${objArrays.affirmations[idx]}`
+
 }
-console.log(randomNumber);
-console.log(generateRandomNumber());
+
+console.log(messageSelector(2, messages));
+
+
+
